50940bc0834a2da60aeb3739534bdef9ab7d3f64
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-11-01 20:18:34 +0900

    fix

--- app/controllers/v1/masters_controller.rb
+++ app/controllers/v1/masters_controller.rb
@@ -1,7 +1,18 @@
 module V1
   class MastersController < ApplicationController
-    def new
+    def get
+    end
 
+    def create
+      @master = ::Master.new master_params
+      if params[:name].blank?
+        render json: { error: "nameを入力してください" }
+      end
+      if @master.save
+        render json: { result: true, message: "masterを作成しました" }
+      else
+        render json: { result: false, message: "masterの保存に失敗しました。" }
+      end
     end
 
     def edit
@@ -13,6 +24,12 @@ module V1
     def index
       @masters = Master.all
       render json: @masters, each_serializer: MastersSerializer
+    end
+
+    private
+
+    def master_params
+      params.require(:master).permit(:name, :id)
     end
   end
 end
